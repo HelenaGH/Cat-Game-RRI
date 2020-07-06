29a15d8cb4409e060631c1f5888bf07daae99ee7
HelenaGH <47184541+HelenaGH@users.noreply.github.com>
2020-07-06 13:43:12 +0200

    CatGame
    -

---
+++ Cat/Cat/Assets/Gems.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gems : MonoBehaviour
+{
+    public int gemValue = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.instance.ChangeScore(gemValue);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Cat/Cat/Assets/scripts/ScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public TextMeshProUGUI text;
+    int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void ChangeScore(int gemValue)
+    {
+        //score += gemValue;
+        text.text = "X" + score.ToString();
+        score = score + gemValue;
+    }
+
+    void FixedUpdate()
+    {
+        if(score == 10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
